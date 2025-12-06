f79266e9cdfc3f36b83894ad2f087d914ad99fcf
Akash Jayaram <akash14035@gmail.com>
2025-12-06 20:30:57 +0530

    update

---
+++ public/firebase-config.js
@@ -0,0 +1,6 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBJsyR0p6ur2djSEppdvvysFFMGe9IqLv0",
+  authDomain: "studio-5934774658-e4401.firebaseapp.com",
+  projectId: "studio-5934774658-e4401",
+};
+firebase.initializeApp(firebaseConfig);
